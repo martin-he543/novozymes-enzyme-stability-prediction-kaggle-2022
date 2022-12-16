af090f208fc57edde8bcb417ded374a2976d775e
Rubén Moreno aguado <68761857+rmaguado@users.noreply.github.com>
2022-12-16 18:40:13 +0000

    Add files via upload

--- model.py
+++ model.py
@@ -1,13 +1,20 @@
 import torch
 import torch.nn as nn
 import math
+import numpy as np
 
 class Transformer(torch.nn.Module):
-    def __init__(self, seq_len, num_heads, dim):
+    def __init__(self, token_dim, seq_len, num_heads, dim):
         super().__init__()
         self.seq_len = seq_len
         self.dim = dim
-        self.attention = nn.MultiheadAttention(dim, num_heads)
+        self.token_dim = token_dim
+        self.query = nn.Linear(token_dim, dim)
+        self.key = nn.Linear(token_dim, dim)
+        self.value = nn.Linear(token_dim, dim)
+        self.attention1 = nn.MultiheadAttention(dim, num_heads, dropout=0.5)
+        #self.attention2 = nn.MultiheadAttention(dim, num_heads)
+        #self.attention3 = nn.MultiheadAttention(dim, num_heads)
         self.fc1 = nn.Sequential(
             nn.Linear(dim, dim),
             nn.ReLU(),
@@ -36,20 +43,27 @@ class Transformer(torch.nn.Module):
         )
         self.fc6 = nn.Linear(seq_len, 1)
 
-        self.positional_encodings = torch.zeros(1, self.seq_len, self.dim)
+        self.positional_encodings = torch.zeros(1, self.seq_len, self.token_dim)
         for pos in range(self.seq_len):
-            for i in range(0, self.dim, 2):
-                self.positional_encodings[0, pos, i] = math.sin(pos / (10000 ** ((2 * i) / self.dim)))
-                self.positional_encodings[0, pos, i + 1] = math.cos(pos / (10000 ** ((2 * i) / self.dim)))
+            for i in range(0, self.token_dim, 2):
+                self.positional_encodings[0, pos, i] = np.sin(pos / (10000 ** ((2 * i) / self.token_dim)))
+                self.positional_encodings[0, pos, i + 1] = np.cos(pos / (10000 ** ((2 * i) / self.token_dim)))
 
     def forward(self, x):
         x += self.positional_encodings
-        attention_output, _ = self.attention(x, x, x)
-        xfc1 = self.fc1(attention_output)
-        xfc1 += attention_output
+        x = torch.transpose(x, 0, 1)
+        print(x.shape)
+        q = self.query(x)
+        k = self.key(x)
+        v = self.value(x)
+        attention_output1, _ = self.attention1(q, k, v)
+        #attention_output2, _ = self.attention2(attention_output1, attention_output1, attention_output1)
+        #attention_output3, _ = self.attention3(attention_output2, attention_output2, attention_output2)
+        xfc1 = self.fc1(attention_output1) #change back to attention_output3
+        xfc1 += attention_output1 #change back to attention_output3
         xfc2 = self.fc2(xfc1)
         xfc2 += xfc1
-        xfc3 = self.fc3(x)
+        xfc3 = self.fc3(xfc2)
         xfc3 += xfc2
         xfc4 = self.fc4(xfc3)
         xfc4 += xfc3
--- train.py
+++ train.py
@@ -2,13 +2,14 @@ import pandas as pd
 import numpy as np
 from model import Transformer
 import torch
+from datetime import datetime
 
 seq_len = 2000
 dim = 512
+token_dim = 4
 num_tokens = 20
 num_heads = 8
 lr = 0.001
-lambda_ = 0.01 #l2 factor
 batch_size = 64
 train_len = 22000
 test_len = 6643
@@ -24,23 +25,22 @@ df_train = df_train.drop(index=drop_indices)
 swap_ph_tm_indices = df_train_updates[~all_features_nan].index
 df_train.loc[swap_ph_tm_indices, ["pH", "tm"]] = df_train_updates.loc[swap_ph_tm_indices, ["pH", "tm"]]
 
-with open("amino_ranking.txt","r") as f:
+with open("amino_ranking.txt", "r") as f:
     amino_codes = f.read().split("\n")
-embeddings = np.random.randn(20,dim)
+embeddings = np.random.randn(20,token_dim)
 
 df_train.drop(df_train[[len(x) > seq_len for x in df_train.protein_sequence]].index, inplace=True)
 all_sequences = df_train["protein_sequence"].values
 all_labels = df_train["tm"].values
 
-model = Transformer(seq_len, num_heads, dim)
+model = Transformer(token_dim, seq_len, num_heads, dim)
 model.train()
-#model.dense.apply(lambda x: x.train(False))
 
-optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 loss_fn = torch.nn.MSELoss()
 
 def get_train_batch():
-    x_sequence = torch.zeros(batch_size, seq_len, dim)
+    x_sequence = torch.zeros(batch_size, seq_len, token_dim)
     x_labels = torch.zeros(batch_size,1)
     indexes = np.random.randint(0,train_len, batch_size)
     for i in range(batch_size):
@@ -51,7 +51,7 @@ def get_train_batch():
     return x_sequence, x_labels
 
 def get_test_batch():
-    x_sequence = torch.zeros(batch_size, seq_len, dim)
+    x_sequence = torch.zeros(batch_size, seq_len, token_dim)
     x_labels = torch.zeros(batch_size,1)
     indexes = np.random.randint(train_len,train_len+test_len, batch_size)
     for i in range(batch_size):
@@ -61,17 +61,30 @@ def get_test_batch():
         x_labels[i] = torch.tensor(all_labels[indexes[i]])
     return x_sequence, x_labels
 
+def save_states(model, optim, epoch):
+    data_dict = {
+        "model_states" : model.state_dict(),
+        "optim_states" : optim.state_dict()
+    }
+    time_obj = datetime.now()
+    path = f"./models/model.e{epoch:04}.{time_obj.month}.{time_obj.day}.{time_obj.hour}:{time_obj.minute}:{time_obj.second}.t"
+    torch.save(data_dict, path)
+
+def load_states(model, optim, path):
+    data_dict = torch.load(path)
+    model.load_state_dict(data_dict["model_states"])
+    optim.load_state_dict(data_dict["optim_states"])
+    return model, optim
+
 total_epochs = 100
 test_interval = 10
+save_interval = 50
 for epoch in range(total_epochs):
     model.train()
     data, labels = get_train_batch()
+    
     output = model(data)
     loss = loss_fn(output, labels)
-
-    # Add the L2 regularization term to the loss
-    for param in model.parameters():
-        loss += lambda_ * torch.norm(param)
 
     optimizer.zero_grad()
     loss.backward()
@@ -83,3 +96,5 @@ for epoch in range(total_epochs):
         eval_loss = loss_fn(output, labels)
         print('Epoch:', epoch, 'Train loss:', loss.item(), "Eval loss:", eval_loss.item())
         model.train()
+    if epoch % save_interval == 0:
+        save_states(model, optimizer, epoch)
