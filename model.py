5c8377fea434d7b4c5933ce98f179f0a204ec000
Rubén Moreno aguado <68761857+rmaguado@users.noreply.github.com>
2022-12-23 11:14:51 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-class EncoderBlock(nn.Module):
+class AttentionBlock(nn.Module):
     def __init__(self, dim, num_heads, fc_dim, dropout):
         super().__init__()
         self.attention = nn.MultiheadAttention(dim, num_heads, batch_first=True, dropout=dropout)
@@ -24,39 +24,35 @@ class EncoderBlock(nn.Module):
         return add_norm2
 
 class Transformer(nn.Module):
-    def __init__(self, token_dim, seq_len, num_heads, dim):
+    def __init__(self, token_dim, seq_len, num_heads, dim, device, dropout=0.5):
         super().__init__()
         self.seq_len = seq_len
         self.dim = dim
         self.token_dim = token_dim
+        self.device = device
         self.scaling = nn.Linear(token_dim, dim)
-        self.attention1 = EncoderBlock(dim, num_heads, fc_dim=dim*2, dropout=0.5)
-        self.attention2 = EncoderBlock(dim, num_heads, fc_dim=dim*2, dropout=0.5)
-        self.attention3 = EncoderBlock(dim, num_heads, fc_dim=dim*2, dropout=0.5)
+        self.attention1 = AttentionBlock(dim, num_heads, fc_dim=dim*2, dropout=dropout)
+        self.attention2 = AttentionBlock(dim, num_heads, fc_dim=dim*2, dropout=dropout)
+        self.attention3 = AttentionBlock(dim, num_heads, fc_dim=dim*2, dropout=dropout)
 
         self.fc1 = nn.Sequential(
             nn.Linear(seq_len, 1),
             nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.Dropout(dropout),
         )
         self.fc2 = nn.Sequential(
             nn.Linear(dim, dim),
             nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.Dropout(dropout),
         )
         self.fc3 = nn.Sequential(
             nn.Linear(dim, dim),
             nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.Dropout(dropout),
         )
-        self.fc4 = nn.Sequential(
-            nn.Linear(dim, 1),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Flatten(),
-        )
+        self.fc4 = nn.Linear(dim, 1)
 
-        self.positional_encodings = torch.zeros(1, self.seq_len, self.token_dim)
+        self.positional_encodings = torch.zeros(1, self.seq_len, self.token_dim).to(self.device)
         for pos in range(self.seq_len):
             for i in range(0, self.token_dim, 2):
                 self.positional_encodings[0, pos, i] = \
@@ -67,11 +63,11 @@ class Transformer(nn.Module):
     def forward(self, x):
         x += self.positional_encodings
         x = self.scaling(x)
-        attention_output1 = self.attention1(x)
-        attention_output2 = self.attention2(attention_output1)
-        attention_output3 = self.attention3(attention_output2)
-        attention_output3 = torch.transpose(attention_output3, 1, 2)
-        xfc1 = self.fc1(attention_output3)
+        x = self.attention1(x)
+        x = self.attention2(x)
+        x = self.attention3(x)
+        x = torch.transpose(x, 1, 2)
+        xfc1 = self.fc1(x)
         xfc1 = torch.transpose(xfc1, 1, 2)
         xfc2 = self.fc2(xfc1)
         xfc2 += xfc1
--- train.py
+++ train.py
@@ -1,19 +1,28 @@
 import pandas as pd
 import numpy as np
 import torch
+import datetime
+from torch.utils.tensorboard import SummaryWriter
+import os
 from model import Transformer
 
+#set sequence constants
 seq_len = 2000
-token_dim = 4
+token_dim = 2
 num_tokens = 20
 
-train_len = 22000
-test_len = 6643
+#set train-test split
+train_len = 25000
+test_len = 3643
 
-dim = 64
+#set transformer properties
+dim = 128
 num_heads = 4
+dropout = 0.5
+
+#set 
 lr = 0.0005
-batch_size = 1
+batch_size = 16
 
 df_train = pd.read_csv("./data/train.csv", index_col="seq_id")
 df_train_updates = pd.read_csv("./data/train_updates_20220929.csv", index_col="seq_id")
@@ -34,15 +43,19 @@ df_train.drop(df_train[[len(x) > seq_len for x in df_train.protein_sequence]].in
 all_sequences = df_train["protein_sequence"].values
 all_labels = df_train["tm"].values
 
-model = Transformer(token_dim, seq_len, num_heads, dim)
-model.train()
+device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+print(device)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+model = Transformer(token_dim, seq_len, num_heads, dim, device, dropout)
+model.to(device)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0.01)
 loss_fn = torch.nn.MSELoss()
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=1000)
 
 def get_train_batch():
-    x_sequence = torch.zeros(batch_size, seq_len, token_dim)
-    x_labels = torch.zeros(batch_size,1)
+    x_sequence = torch.zeros(batch_size, seq_len, token_dim).to(device)
+    x_labels = torch.zeros(batch_size, 1, 1).to(device)
     indexes = np.random.randint(0,train_len, batch_size)
 
     for i in range(batch_size):
@@ -55,8 +68,8 @@ def get_train_batch():
     return x_sequence, x_labels
 
 def get_test_batch():
-    x_sequence = torch.zeros(batch_size, seq_len, token_dim)
-    x_labels = torch.zeros(batch_size,1)
+    x_sequence = torch.zeros(batch_size, seq_len, token_dim).to(device)
+    x_labels = torch.zeros(batch_size, 1, 1).to(device)
     indexes = np.random.randint(train_len,train_len+test_len, batch_size)
     for i in range(batch_size):
         x_raw = np.array([embeddings[amino_codes.index(x)] \
@@ -66,36 +79,47 @@ def get_test_batch():
         x_labels[i] = torch.tensor(all_labels[indexes[i]])
     return x_sequence, x_labels
 
-def save_states(model, optim, epoch):
+def save_states(model, optim, epoch, train_loss, path):
     data_dict = {
         "model_states" : model.state_dict(),
-        "optim_states" : optim.state_dict()
+        "optim_states" : optim.state_dict(),
+        "epoch" : epoch,
+        "train_loss": train_loss
     }
-    path = f"./models/model.torch"
     torch.save(data_dict, path)
 
-def load_states(model, optim, path):
-    data_dict = torch.load(path)
-    model.load_state_dict(data_dict["model_states"])
-    optim.load_state_dict(data_dict["optim_states"])
-    return model, optim
+#tensorboard
+log_dir = './runs/{:%Y.%m.%d.%H.%M.%S}'.format(datetime.datetime.now())
+writer = SummaryWriter(log_dir=log_dir)
 
-total_epochs = 10
+resume_epoch = 0
+#comment the following to not resume
+
+load_dict = torch.load("./models/2022.12.22.18.06.38.torch")
+model.load_state_dict(load_dict["model_states"])
+optimizer.load_state_dict(load_dict["optim_states"])
+resume_epoch = load_dict["epoch"]
+for g in optimizer.param_groups:
+    g['lr'] = lr
+
+total_epochs = 100000
 train_batches = 100
 eval_batches = 10
 best_loss = 10000
-for epoch in range(total_epochs):
+best_saves = []
+last_saves = []
+for epoch in range(resume_epoch, total_epochs):
     model.train()
+    optimizer.zero_grad()
     train_loss = 0
     for _ in range(train_batches):
         data, labels = get_train_batch()
         output = model(data)
         loss = loss_fn(output, labels)
         train_loss += loss.item()
-        optimizer.zero_grad()
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
-        optimizer.step()
+    #torch.nn.utils.clip_grad_norm_(model.parameters(), 5)
+    optimizer.step()
     train_loss /= train_batches
     model.eval()
     eval_loss = 0
@@ -105,6 +129,24 @@ for epoch in range(total_epochs):
         loss = loss_fn(output, labels)
         eval_loss += loss.item()
     eval_loss /= eval_batches
+    scheduler.step(train_loss)
+    for param_group in optimizer.param_groups:
+        current_lr = param_group['lr']
+    writer.add_scalar('Train loss', round(train_loss,4), global_step=epoch)
+    writer.add_scalar('Eval loss', round(eval_loss,4), global_step=epoch)
+    writer.add_scalar('Learning rate', current_lr, global_step=epoch)
     print('Epoch:', epoch, 'Train loss:', train_loss, "Eval loss:", eval_loss)
-    if eval_loss < best_loss:
-        save_states(model, optimizer, epoch)
+    timestamp = '{:%Y.%m.%d.%H.%M.%S}'.format(datetime.datetime.now())
+    path = f"./models/{timestamp}.torch"
+    if train_loss < best_loss:
+        best_saves.append(path)
+        if len(best_saves) > 5:
+            os.remove(best_saves[0])
+            del(best_saves[0])
+        best_loss = train_loss
+    else:
+        last_saves.append(path)
+        if len(last_saves) > 5:
+            os.remove(last_saves[0])
+            del(last_saves[0])
+    save_states(model, optimizer, epoch, train_loss, path)
